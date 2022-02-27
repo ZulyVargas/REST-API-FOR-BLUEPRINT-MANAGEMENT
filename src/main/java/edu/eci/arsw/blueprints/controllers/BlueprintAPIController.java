f48966216d7ac05705548060f11d42d31f93f43b
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-02-26 23:57:39 -0500

    Implementación: Post

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -29,51 +29,64 @@ import static java.util.logging.Level.*;
 @RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
 
- @Autowired
- BlueprintsServices bps;
+    @Autowired
+    BlueprintsServices bps;
 
 
- @RequestMapping(method = RequestMethod.GET)
- public ResponseEntity<?> getBluePrintsInJSON(){
-      try {
-       //obtener datos que se enviarán a través del API
-           return new ResponseEntity<>(bps.getAllBlueprints(), HttpStatus.ACCEPTED);
-       } catch (Exception ex) {
-           Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
-           return new ResponseEntity<>("Error bla bla bla",HttpStatus.NOT_FOUND);
-       }
- }
-
- @RequestMapping( value = "/{author}" , method = RequestMethod.GET)
- public ResponseEntity<?> getBluePrintsByAuthorInJSON(@PathVariable("author") String author){
-      try {
-       //obtener datos que se enviarán a través del API dado el autor
-           Set<Blueprint> results = bps.getBlueprintsByAuthor(author);
-           if (results.isEmpty()){
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-           }
-           return new ResponseEntity<>(results, HttpStatus.ACCEPTED);
-      } catch (Exception ex) {
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getBluePrintsInJSON() {
+        try {
+            //obtener datos que se enviarán a través del API
+            return new ResponseEntity<>(bps.getAllBlueprints(), HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
             Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
-            return new ResponseEntity<>("Error bla bla bla",HttpStatus.NOT_FOUND);
-  }
- }
-
-
-@RequestMapping( value = "/{author}/{bpname}" , method = RequestMethod.GET)
-public ResponseEntity<?> getBluePrintsByAuthorAndNameInJSON(@PathVariable("author") String author,@PathVariable("author") String bpname ){
-    try {
-        //obtener datos que se enviarán a través del API dado el autor
-        Set<Blueprint> results = bps.getBlueprintsByAuthor(author);
-        if (results.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Error al consultar todos los planos", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(results.toArray()[0], HttpStatus.ACCEPTED);
-    } catch (Exception ex) {
-        Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
-        return new ResponseEntity<>("Error bla bla bla",HttpStatus.NOT_FOUND);
     }
-}
+
+    @RequestMapping(value = "/{author}", method = RequestMethod.GET)
+    public ResponseEntity<?> getBluePrintsByAuthorInJSON(@PathVariable("author") String author) {
+        try {
+            //obtener datos que se enviarán a través del API dado el autor
+            Set<Blueprint> results = bps.getBlueprintsByAuthor(author);
+            if (results.isEmpty()) {
+                return new ResponseEntity<>("NO ENCONTRADO",HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(results, HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error al consultar por autor", HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+    @RequestMapping(value = "/{author}/{bpname}", method = RequestMethod.GET)
+    public ResponseEntity<?> getBluePrintsByAuthorAndNameInJSON(@PathVariable("author") String author, @PathVariable("author") String bpname) {
+        try {
+            //obtener datos que se enviarán a través del API dado el autor y nombre del plano
+            Set<Blueprint> results = bps.getBlueprintsByAuthor(author);
+            if (results.isEmpty()) {
+                return new ResponseEntity<>("NO ENCONTRADO", HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(results.toArray()[0], HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error al consultar por autor y nombre plano", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    public ResponseEntity<?> addBluePrintsWithJSON(@RequestBody Blueprint blueprint) {
+        try {
+            //Enviar datos de creación para el plano
+            bps.addNewBlueprint(blueprint);
+            return new ResponseEntity<>("CREADO :D", HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error al crear nuevo plano ", HttpStatus.NOT_FOUND);
+        }
+    }
 
 
 
